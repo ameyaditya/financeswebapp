17e2eaa81ae349bb71091a79a558ef77e76efe4d
Adithyakaushik326 <adithyakaushik326@gmail.com>
2019-11-29 23:09:00 +0530

    @media for index

--- Final webapp/index.php
+++ Final webapp/index.php
@@ -27,6 +27,8 @@ session_start();
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 	<style type="text/css">
+		@media screen and (min-width: 700px)
+		{
 		.container {
 			margin-top: 150px;
 			border-radius: 15px;
@@ -34,6 +36,27 @@ session_start();
 			width: 500px;
 			box-shadow:5px 5px 10px 5px rgba(0,0,0,.4) ;
 		}
+		.container-fluid
+		{
+			color: white;
+			font-size: 2em;
+		}
+	}
+			@media screen and (max-width: 700px)
+		{
+		.container {
+			margin-top: 120px;
+			border-radius: 15px;
+			height: 250px;
+			width: 450px;
+			box-shadow:5px 5px 10px 5px rgba(0,0,0,.4) ;
+		}
+		.container-fluid
+		{
+			color: white;
+			font-size: 1.5em;
+		}
+	}
 		#header
 		{
 			/*padding-top: 30px;*/
@@ -66,8 +89,8 @@ session_start();
 </head>
 
 <body style="background-color: #f5f5f5;">
-	<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-		<div class="container-fluid" style="color: white;font-size: 2em">Amity Ramapriya Finances
+	<nav class="navbar navbar-expand navbar-dark bg-dark">
+		<div class="container-fluid" >Amity Ramapriya Finances
 		</div>
 	</nav>
 	<div class="container ">
